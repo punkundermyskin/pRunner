59c3250535dbfc8e203b6c804e2d3b1eede6d2b2
punkundermyskin <p.nkundermyskin@gmail.com>
2019-11-03 20:27:53 +0300

    Initial Commit

---
+++ pRunner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pRunner
+//
+//  Created by pennyworth on 11/3/19.
+//  Copyright © 2019 punkundermyskin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
